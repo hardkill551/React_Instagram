ff1e38678cd3f562ddfea4db3ff7e0bd473ccc1c
hardkill <hardkill194@email.com>
2023-02-10 21:08:06 -0300

    Testando tudo agora

--- src/components/corpo/esquerda/posts/conteudo.js
+++ src/components/corpo/esquerda/posts/conteudo.js
@@ -2,21 +2,23 @@ import { useState } from "react"
 
 export default function Conteudo(props) {
   const [curtidas, setCurtidas] = useState(101)
+  const [preencher, setPreencher] = useState(false)
   const x = 101
+  
     return (
       <>
       <div className="conteudo">
       <img data-test="post-image" onDoubleClick={() => setCurtidas(adicionarCurtida(curtidas, x))} src={props.imagePost} alt={props.nomeImagem}/>
       </div>
         <div className="fundo">
-        <div className="acoes">
+        <div className="acoes"> 
           <div>
-            <ion-icon data-test="like-post" onClick={() => setCurtidas(adicionarCurtidas(curtidas, x))} name="heart-outline"></ion-icon>
+            <ion-icon data-test="like-post" onClick={() => setCurtidas(adicionarCurtidas(curtidas, x))} class={`${curtidas==101 ? "" : "vermelho"}`} name={`heart${curtidas==101 ? "-outline" : ""}`}></ion-icon>
             <ion-icon name="chatbubble-outline"></ion-icon>
             <ion-icon name="paper-plane-outline"></ion-icon>
           </div>
           <div>
-            <ion-icon data-test="likes-number" name="bookmark-outline"></ion-icon>
+            <ion-icon onClick={() => setPreencher(preencherSalvar(preencher))}data-test="likes-number" name={`bookmark${preencher==false ? "-outline" : ""}`}></ion-icon>
           </div>
         </div>
         <div className="curtidas">
@@ -44,4 +46,11 @@ function adicionarCurtida(curtidas, x){
     return curtidas
   }
   return curtidas+1
+}
+
+function preencherSalvar(preencher){                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+  if(preencher==false){
+    return true
+  }
+  return false
 }
